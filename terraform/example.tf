f907ac54d79a152b8ba8a15da873799f383cdf6f
Eric Cestari <ecestari@mac.com>
2019-03-14 15:58:17 +0000

    user_data

--- terraform/example.tf
+++ terraform/example.tf
@@ -11,11 +11,20 @@ data "aws_ami" "web" {
   }
 }
 
+
+
+
 resource "aws_instance" "example" {
-  ami           = "${data.aws_ami.web.id}"
-  instance_type = "a1.medium"
+  ami           = "ami-0be70fe886f1bdc4b" #"${data.aws_ami.web.id}"
+  instance_type = "t2.medium"
   key_name      = "fenwick"
   vpc_security_group_ids = ["sg-0db204212259fa981"]
   subnet_id     = "subnet-3f880c59"
   iam_instance_profile =    "AWSCloudWatchAgent"
+
+  user_data =<<EOT
+#!/bin/sh
+/opt/denyall/sbin/toManaged.sh
+EOT
+
 }
